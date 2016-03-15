a9d5256a9d1f08f874c69e0bf7f003236021c04d
Adam Wulf <adam.wulf@gmail.com>
2016-03-14 23:35:56 -0500

    Initial Commit

---
+++ simple-nn/simple-nn.h
@@ -0,0 +1,19 @@
+//
+//  simple-nn.h
+//  simple-nn
+//
+//  Created by Adam Wulf on 3/14/16.
+//  Copyright © 2016 Milestone Made. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for simple-nn.
+FOUNDATION_EXPORT double simple_nnVersionNumber;
+
+//! Project version string for simple-nn.
+FOUNDATION_EXPORT const unsigned char simple_nnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simple_nn/PublicHeader.h>
+
+
